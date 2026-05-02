d1b78153d2dd73d7ef6731a1fbbc39b56424467d
David Badell <davidbadell42@gmail.com>
2026-05-02 10:24:47 -0400

    feat: integrate BottomNav into AppSidebarLayout with mobile padding

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -2,6 +2,7 @@ import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { BottomNav } from '@/components/bottom-nav';
 import { type BreadcrumbItem } from '@/types';
 import { type PropsWithChildren } from 'react';
 
@@ -14,8 +15,11 @@ export default function AppSidebarLayout({
             <AppSidebar />
             <AppContent variant="sidebar" className="overflow-x-hidden">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                {children}
+                <div className="pb-16 md:pb-0">
+                    {children}
+                </div>
             </AppContent>
+            <BottomNav />
         </AppShell>
     );
 }
