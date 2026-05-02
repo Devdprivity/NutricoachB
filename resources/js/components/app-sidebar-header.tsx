90cecf670cf2c0380a73738cda754f328de308ef
David Badell <davidbadell42@gmail.com>
2026-05-02 10:25:52 -0400

    feat: hide SidebarTrigger on mobile (bottom nav replaces it)

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -39,7 +39,9 @@ export function AppSidebarHeader({
     return (
         <header className="relative flex h-16 shrink-0 items-center justify-between gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
             <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
+                <div className="hidden md:flex">
+                    <SidebarTrigger className="-ml-1" />
+                </div>
                 <Breadcrumbs breadcrumbs={breadcrumbs} />
             </div>
 
